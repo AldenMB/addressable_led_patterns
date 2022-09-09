768e02f54420107163f9b54548599fe6dbf0457a
Alden Bradford <aldenbradford@gmail.com>
2022-09-09 14:17:17 -0400

    use broadcasting hsv_to_rgb

--- led_patterns.py
+++ led_patterns.py
@@ -1,12 +1,12 @@
 """ Patterns suitable for showing on an LED string. Also, a simple tool for showing them animated in a notebook. No gamma correction or dimming is done here, as they should be custom to the light string used. To get the animations make sure you use the widget backend, with %matplotlib widget."""
 
-import colorsys
 import functools
 import threading
 import time
 
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.colors import hsv_to_rgb
 
 patterns = {}
 
@@ -47,10 +47,11 @@ def show(pattern):
 
 
 def rainbow(length=55, saturation=0.75, value=1):
-    data = np.zeros((length, 3), dtype=int)
-    for i, hue in enumerate(np.linspace(0, 1, length)):
-        data[i] = 256 * np.array(colorsys.hsv_to_rgb(hue, saturation, value))
-    return data
+    hsv = np.zeros((length, 3), dtype=float)
+    hsv[:, 0] = np.linspace(0, 1, length, endpoint=False)
+    hsv[:, 1] = saturation
+    hsv[:, 2] = value
+    return 256 * hsv_to_rgb(hsv)
 
 
 @pattern
@@ -75,12 +76,11 @@ def color_impulse(length, rng, renewal_rate):
     so as to follow an exponential distribution. Choose the hue uniformly at random.
     """
     locations = rng.random(length) > np.exp(-renewal_rate)
-    hue = rng.random(locations.sum())
-    rgb = np.zeros((len(hue), 3), dtype=float)
-    for i, h in enumerate(hue):
-        rgb[i] = colorsys.hsv_to_rgb(h, 1, 1)
+    num_locations = locations.sum()
+    hsv = np.ones((num_locations, 3), dtype=float)
+    hsv[:, 0] = rng.random(num_locations)
     result = np.zeros((length, 3), dtype=float)
-    result[locations] = rgb
+    result[locations] = hsv_to_rgb(hsv)
     return result
 
 
@@ -101,7 +101,7 @@ def diffusion(
 
 
 @pattern
-def waves(length=55, dt=0.05, renewal_rate=0.01, impulse=8, wave_speed=2, damping=0.3):
+def waves(length=55, dt=0.05, renewal_rate=0.005, impulse=8, wave_speed=2, damping=0.3):
     """Models the 1-dimensional wave equation."""
 
     rng = np.random.default_rng()
@@ -115,35 +115,39 @@ def waves(length=55, dt=0.05, renewal_rate=0.01, impulse=8, wave_speed=2, dampin
         time.sleep(dt)
 
 
-def random_saturated(rng, length):
-    """give a color with a random hue"""
-    out = np.zeros((length, 3))
-    hues = rng.random(length)
-    for i, hue in enumerate(hues):
-        out[i] = colorsys.hsv_to_rgb(hue, 1, 1)
-    return out
-
-
 @pattern
-def bicubic_path(length=55, dt=0.05, time_between_targets=2):
+def bicubic_path(length=55, dt=0.05, time_between_targets=2, tension=0):
     """Move the bar through spacetime, interpolating so as to hit randomly chosen
     points at regular intervals while maintaining smoothness in space and time.
 
     The idea is to have a string-position parameter s with 0<s<3 and time parameter
     with 0<t<3. We specify the colors on the grid where s=0, 1, 2, 3 and t=0, 1, 2, 3.
-    Then we interpolate values for s to fill each pixel and each time t=1, 1+dt, 1+2dt, ... 2.
-    Once we reach t=2, we drop the values for t=0 and shift the grid down by 1, introducing
-    new values for t=3.
-    
+    Then we interpolate values for s to fill each pixel for 0<s<3 and each time
+    t=1, 1+dt, 1+2dt, ... 2. Once we reach t=2, we drop the values for t=0 and shift
+    the grid down by 1, introducing new values for t=3.
+
     A better way to do this would be to interpolate the values at t=1 and 2, and use the
     values at t=0 and 3 to specify the derivatives at t=1 and 2. That would make the movement
     smooth in time as well as space -- currently it is only smooth in space and continuous
-    in time.
+    in time. Since the derivative f_t(t=2, s) is cubic in s, it is completely specified by
+    its values at four points, so it will be continuous when we shift the values.
+
+    We can do that with a tension parameter, taking the slope at (say, s=0, t=1) to be
+    df/dt(0, 1) = (1-tension)(f(0,2)-f(0,1))/2. When the tension is 0, this is exactly
+    the slope of the secant line. When the tension is 1, the slope is just constrained
+    to be zero at t=1 and t=2. When the tension is 0.5, the spline is a centripetal
+    catmull-rom spline.
+
+    We could do the same for s, which would allow us to have many control points
+    along the path. For a string without many pixels, that would diminish the effect
+    of the smoothness -- we would lose it to the discrete nature of the display.
     """
     rng = np.random.default_rng()
-    grid = np.zeros((4, 4, 3), dtype=float)
-    for i in range(4):
-        grid[i] = random_saturated(rng, 4)
+    hues = np.ones((4, 4, 3), dtype=float)
+    hues[:, :, 0] = rng.random((4, 4))
+    grid = hsv_to_rgb(hues)
+    hues = np.ones((4, 3), dtype=float)
+
     powers = np.arange(4).reshape(-1, 1)
     X = np.arange(4) ** powers
     Xinv = np.linalg.inv(X)
@@ -152,9 +156,10 @@ def bicubic_path(length=55, dt=0.05, time_between_targets=2):
     t = np.linspace(1, 2, int(time_between_targets / dt), endpoint=False) ** powers
     Tinv_t = Xinv @ t
     while True:
-        paths = 256 * np.einsum("ij, jkl, km -> iml", Tinv_t.T, ends, Sinv_s)
+        paths = 256 * np.einsum("ij, jkl, km -> iml", Tinv_t.T, grid, Sinv_s)
         for path in paths:
             yield path
             time.sleep(dt)
         grid = np.roll(grid, shift=-1, axis=0)
-        grid[-1] = random_saturated(rng, 4)
+        hues[:, 0] = rng.random(4)
+        grid[-1] = hsv_to_rgb(hues)
